2d0ee87839a9ffef5e289ed46700495813d0565d
Mervin Hernandez <me@mervinhernandez.com>
2016-12-30 15:50:38 -0500

    CREATE the repo

---
+++ mhpk-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: DJMOrtiz.com - Customizations
+Plugin URI: http://piklist.com
+Description: Adds the custom PikList-enabled functionality and features to my site
+Version: 0.0.1
+Author: Mervin Hernandez
+Author URI: http://mervinhernandez.com
+Plugin Type: Piklist
+Text Domain: djmopk
+*/
---
+++ parts/dashboard/dash-djmo.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Title: DJMOrtiz.com
+Role: administrator, editor
+Capability: edit_posts
+*/
+echo 'Content of the DJMOrtiz status box goes here.';
